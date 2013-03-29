4f3d40b99fa8d54b40c85e878839dbda1339409f
Ali Nabavi <github@alijnabavi.info>
2013-03-29 09:16:26 -0400

    - Added CreateCause method to CausesController to start creating new causes - Added "name" property to Cause and Effect fixture data - Removed class assignments in index.html to change display of causes/effects from grid to list - Created effects template - Added logic from TodoMVC in index.html to allow creation of new data

--- js/app.js
+++ js/app.js
@@ -12,9 +12,35 @@ App.Router.map(function() {
     });
 });
 
+// Controllers
+App.CausesController = Ember.ArrayController.extend({
+    createCause: function() {
+        // Get the cause title set by the "New Name" text field
+        var title = this.get('newName');
+        if (!title.trim()) { return; }
+
+        // Create the new Cause model
+        App.Cause.createRecord({
+            name: title,
+        });
+
+        // Clear the "New Cause" text field
+        this.set('newName', '');
+
+        // Save the new model
+        this.get('store').commit();
+    }
+})
+
 App.CausesRoute = Ember.Route.extend({
     model: function() {
         return App.Cause.find();
+    }
+});
+
+App.EffectsRoute = Ember.Route.extend({
+    model: function() {
+        return App.Effect.find();
     }
 });
 
@@ -25,39 +51,49 @@ App.Store = DS.Store.extend({
 });
 
 App.Cause = DS.Model.extend({
-    effects: DS.hasMany('App.Effect')
+    effects: DS.hasMany('App.Effect'),
+    name: DS.attr('string')
 });
 
 App.Effect = DS.Model.extend({
-    causes: DS.hasMany('App.Cause')
+    causes: DS.hasMany('App.Cause'),
+    name: DS.attr('string')
 });
 
 // Fixture data
 App.Cause.FIXTURES = [{
     id: 1,
-    effect: 1
+    effect: 1,
+    name: 'Getting too little sleep'
 }, {
     id: 2,
-    effect: 2
+    effect: 2,
+    name: 'Eating too much food'
 }, {
     id: 3,
-    effect: 3
+    effect: 3,
+    name: 'Reading a good book'
 }, {
     id: 4,
-    effect: 4
+    effect: 4,
+    name: 'Going to the gym'
 }];
 
 App.Effect.FIXTURES = [{
     id: 1,
-    cause: 1
+    cause: 1,
+    name: 'Gaining weight'
 }, {
     id: 2,
-    cause: 2
+    cause: 2,
+    name: 'Getting sleepy in the afternoon'
 }, {
     id: 3,
-    cause: 3
+    cause: 3,
+    name: 'Losing concentration at work'
 }, {
     id: 4,
-    cause: 4
+    cause: 4,
+    name: 'Feeling in a better mood'
 }];
 
